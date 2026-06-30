551b75f3c9ee59ab05e8036a515fbf8cf12e43f3
bhavana4901-rgb <bhavanaaddepalli2001@gmail.com>
2026-06-30 15:30:41 +0530

    Create Loader component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center py-12">
+      <div className="w-12 h-12 border-4 border-blue-200 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  )
+}
+
+export default Loader
